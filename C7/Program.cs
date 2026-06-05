ea99e551320c0fee3769a4a2c9c0cac1a16d4372
Anakin <johninjug56@gmail.com>
2026-06-05 23:21:29 +0800

    Removed Program.cs secrets, and changed app script link

--- C7/Program.cs
+++ C7/Program.cs
@@ -12,26 +12,9 @@ namespace C7
         [STAThread]
         static void Main()
         {
-            //EnsureStartup();
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-        //private static void EnsureStartup()
-        //{
-        //    string appName = "C7";
-        //    string exePath = Application.ExecutablePath;
-
-        //    RegistryKey key = Registry.CurrentUser.OpenSubKey(
-        //        @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
-
-        //    object currentValue = key.GetValue(appName);
-
-        //    if (currentValue == null || currentValue.ToString() != exePath)
-        //    {
-        //        key.SetValue(appName, exePath);
-        //    }
-        //}
     }
 }
